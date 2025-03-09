56f6dad31d3591d3c9dc71ed2736bdb4452c042a
adfhsjt <ahmadriski207@gmail.com>
2025-03-09 21:01:13 +0700

    Relationships

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserModel extends Model
 {
@@ -13,4 +15,51 @@ class UserModel extends Model
     protected $primaryKey = 'user_id';
 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+}
+
+// //One to One
+// class UserModel extends Model
+// {
+//     public function level(): BelongsTo
+//     {
+//         return $this->belongsTo(LevelModel::class);
+//     }
+// }
+
+// Mendefinisikan Kebalikan dari Hubungan One-to-one
+class LevelModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_level'; // Pastikan ini sesuai dengan nama tabel di database
+    protected $primaryKey = 'level_id'; // Primary key tabel
+
+    protected $fillable = ['level_nama', 'level_kode'];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(UserModel::class, 'level_id', 'level_id');
+    }
+}
+
+//One to Many
+class KategoriModel extends Model
+{
+    public function barang(): HasMany
+    {
+        return $this->hasMany(BarangModel::class, 'barang_id', 'barang_id');
+    }
+}
+
+// One to Many (Inverse) / Belongs To
+class BarangModel extends Model
+{
+    public function kategori(): BelongsTo
+    {
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,6 +7,8 @@
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <th>Kode Level</th>
+            <th>Nama Level</th>
             <th>Aksi</th>
         </tr>
         {{-- <tr>
@@ -18,6 +20,8 @@
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->level_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
                 <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus
                 </td>
             </tr>
