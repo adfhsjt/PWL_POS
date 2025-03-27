6b77729d79803fb2bc53db5b3562a125806f19c0
adfhsjt <ahmadriski207@gmail.com>
2025-03-27 09:10:05 +0700

    Tugas 4 – Implementasi Form Registrasi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\UserModel;
 
 class AuthController extends Controller
 {
@@ -42,5 +43,37 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('login');
+    }
+
+    public function register()
+    {
+        Auth::logout();
+        return view('auth.register');
+    }
+
+    public function postregister(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password),
+            'level_id' => 5,
+        ]);
+
+        if ($request->ajax() || $request->wantsJson()) {
+            $credentials = $request->only('username', 'password');
+            if (Auth::attempt($credentials)) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Daftar Berhasil',
+                    'redirect' => url('/')
+                ]);
+            }
+            return response()->json([
+                'status' => false,
+                'message' => 'Login Gagal'
+            ]);
+        }
+        return redirect('/');
     }
 }
